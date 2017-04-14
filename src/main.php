b18ca4c6cb957caa07dd7743379ef5d4b68cf3b9
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2017-04-14 12:49:08 -0500

    Updated version. Defined HTML5 node inside main class.

--- src/main.php
+++ src/main.php
@@ -9,9 +9,63 @@
  * License, or (at your option) any later version.
  */
 
+namespace GIgenerator\DML\HTML5;
+
 require_once __DIR__ . '/../../DML/dist/DML.phar';
 
-require_once __DIR__ . '/main/Node.php';
+
+require_once __DIR__ . '/main/Handlers/Classes.php';
+require_once __DIR__ . '/main/Handlers/Scripts.php';
+
+/**
+ * Represents an <b>HTML5</b> node.
+ * 
+ * 
+ * @package     HTML5
+ * @subpackage  Main
+ * @category    API
+ * 
+ * @copyright   (c) 2017 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @version     GI-DML.01
+ * @since       2016-12-21
+ * @author      Angel Sierra Vega <angel.sierra@grupoindie.com>
+ * 
+ */
+class Node extends \GIgenerator\DML\Node {
+    
+    use Handlers\classHandler;
+    use Handlers\scriptHandler;
+    
+    
+    /**
+     * Calls internal method addClass on trying to add a class attribute.
+     * 
+     * See internal method {@see \GIgenerator\DML\HTML5\Handlers\Classes::addClass()}
+     * See parent method {@see \GIgenerator\DML\Node\Tag\OpenTag::setAttribute()}
+     * 
+     * @param       string $attributeName
+     * @param       string $value [optional]
+     * 
+     * @return      boolean TRUE if setted. FALSE otherwize
+     * @throws      NA
+     * 
+     * @version     GI-DML.01.01
+     * @since       2016-12-21
+     * @author      Angel Sierra Vega <angel.sierra@grupoindie.com>
+     */
+    public function setAttribute($attributeName, $value = NULL) {
+        switch ($attributeName) {
+            case "class":
+                return $this->addClass($value);
+                break;
+            default:
+                return parent::setAttribute($attributeName, $value);
+                break;
+        }
+    }
+
+}
 
 require_once __DIR__ . '/main/Categories/Basic.php';
 require_once __DIR__ . '/main/Categories/Links.php';
