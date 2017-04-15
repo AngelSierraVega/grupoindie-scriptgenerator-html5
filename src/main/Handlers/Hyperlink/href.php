342ac094e2a08353003c88e39c07973e84a9eb7c
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2017-04-15 16:24:46 -0500

    Created documents

---
+++ src/main/Handlers/Hyperlink/href.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Copyright (C) 2017 Angel Sierra Vega. Grupo INDIE.
+ *
+ * This software is protected under GNU: you can use, study and modify it
+ * but not distribute it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ */
+
+namespace GIgenerator\DML\HTML5\Handlers\Hyperlink;
+
+/**
+ * Specifies the URL of the page the link goes to.
+ *
+ * @package     HTML5
+ * @subpackage  Main
+ * @category    API
+ * 
+ * @copyright   (c) 2017 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @version     GI-HTML5.00
+ * @since       2017-04-15
+ * @author      Angel Sierra Vega <angel.sierra@grupoindie.com>
+ * 
+ */
+trait hrefTrait {
+
+    public function setHref($href) {
+        return $this->setAttribute("href", $href);
+    }
+
+}
+
+interface href {
+
+    public function setHref($href);
+}
